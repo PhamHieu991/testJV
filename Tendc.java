8c626e334acc824a23148b279214f0dae7ed38d4
PhamHieu991 <hieudien7891@gmail.com>
2025-03-13 19:11:37 +0700

    TestJV

---
+++ Tendc.java
@@ -0,0 +1,8 @@
+public class Tendc {
+    public static void main(String[] args) {
+        String name = "Hieu";
+        String dc = "Dong Da, Ha Noi";
+        System.out.println("Ten: " + name);
+        System.out.println("Dia chi: " + dc);
+    }
+}
